0efa49a6d0600fbc9817299088a114df944bc1ce
Lharrison107 <lharrison@diesellaptops.com>
2022-07-07 14:45:37 -0400

    part 33 completed

---
+++ quasar-project/src/pages/PageSettings.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding>
+    <p>Settings Page</p>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    data() {
+      
+    },
+    methods: {
+     
+    },
+    components: {
+      
+    }
+  }
+</script>
+
+<style>
+  
+</style>
---
+++ quasar-project/src/pages/PageTodo.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding>
+    <p>Todo Page</p>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    data() {
+      
+    },
+    methods: {
+     
+    },
+    components: {
+      
+    }
+  }
+</script>
+
+<style>
+  
+</style>
