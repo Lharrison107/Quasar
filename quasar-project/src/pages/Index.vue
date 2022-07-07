51d6c1f48962c3d5d67780cd2924da2079d25ef7
Lharrison107 <lharrison@diesellaptops.com>
2022-07-07 13:23:43 -0400

    parts 25 - 27 completed

--- quasar-project/src/pages/Index.vue
+++ quasar-project/src/pages/Index.vue
@@ -1,9 +1,12 @@
 <template>
   <q-page padding>
     <li
-      v-for="task in tasks"
+      v-for="(task, index) in tasks"
     >
-      {{ task }}
+      <div>{{ task.name }} {{ index }}</div>
+      <hr>
+      <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+      <button @click="deleteTask(index)">X</button>
     </li>
   </q-page>
 </template>
@@ -12,7 +15,28 @@
   export default {
     data() {
       return {
-        tasks : ['Go to shop', 'Get bananas', 'Get apples']
+        tasks : [
+          {
+            name: 'Got to shop',
+            dueDate: '2022/27/09',
+            dueTime: '14:00'
+          },
+          {
+            name: 'Get bananas',
+            dueDate: '2022/27/10',
+            dueTime: '14:00'
+          },
+          {
+            name: 'Get apples',
+            dueDate: '2022/27/19',
+            dueTime: '14:00'
+          }
+        ]
+      }
+    },
+    methods: {
+      deleteTask(index) {
+        this.tasks.splice(index, 1)
       }
     }
   }
