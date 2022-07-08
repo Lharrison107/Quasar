087df223ecbc4d5c01dd676a09768d4877627631
Lharrison107 <lharrison@diesellaptops.com>
2022-07-08 15:42:58 -0400

    part 43 completed use ref() instead of just true or false in checked booleen

--- quasar-project/src/pages/PageTodo.vue
+++ quasar-project/src/pages/PageTodo.vue
@@ -1,11 +1,51 @@
 <template>
-  <q-page padding>
+  <q-page class="q-pa-md">
     <p>Todo Page</p>
+
+     <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
   </q-page>
 </template>
 
 <script>
+import { ref } from 'vue'
+
   export default {
+    data() {
+      return {
+        tasks: [
+           {
+              id: 1,
+              name: 'Go to store',
+              completed: ref(false),
+            },
+            {
+              id: 2,
+              name: 'Get bananas',
+              completed: ref(false),
+            },
+            {
+              id: 3,
+              name: 'Get apples',
+              completed: ref(false),
+            }
+        ]
+      }
+    }
   }
 </script>
 
