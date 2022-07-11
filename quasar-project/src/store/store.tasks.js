f2ef3fd16c17f1169130326dabd649648dd0012c
Lharrison107 <lharrison@diesellaptops.com>
2022-07-11 11:30:24 -0400

    part 49 completed.

--- quasar-project/src/store/store.tasks.js
+++ quasar-project/src/store/store.tasks.js
@@ -1,5 +1,27 @@
 const state = {
-
+    tasks: [
+        {
+           id: 1,
+           name: 'Go to store',
+           completed: false,
+           dueDate: '7/10/25',
+           dueTime: '14:00'
+         },
+         {
+           id: 2,
+           name: 'Get bananas',
+           completed: false,
+           dueDate: '7/10/23',
+           dueTime: '14:00'
+         },
+         {
+           id: 3,
+           name: 'Get apples',
+           completed: false,
+           dueDate: '7/10/29',
+           dueTime: '14:00'
+         }
+    ]
 }
 
 const mutations = {
