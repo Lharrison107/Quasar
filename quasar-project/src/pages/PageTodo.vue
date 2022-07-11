6cf9333bdf2e60ab2b04a7d66b2fca3e645c1b0a
Lharrison107 <lharrison@diesellaptops.com>
2022-07-11 08:15:11 -0400

    part 48 complete

--- quasar-project/src/pages/PageTodo.vue
+++ quasar-project/src/pages/PageTodo.vue
@@ -1,36 +1,39 @@
 <template>
   <q-page class="q-pa-md">
     <p>Todo Page</p>
-
-     <q-item
-        v-for="task in tasks"
-        :key="task.id"
-        @click="task.completed = !task.completed"
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-        clickable
-        v-ripple
+      <q-list
+        separator
+        bordered
       >
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
+        <q-item
+          v-for="task in tasks"
+          :key="task.id"
+          @click="task.completed = !task.completed"
+          :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+          clickable
+          v-ripple
+        >
+          <q-item-section side top>
+            <q-checkbox v-model="task.completed" />
+          </q-item-section>
 
-        <q-item-section>
-          <q-item-label
-            :class="{ 'text-strike' : task.completed }"
-          >
-            {{ task.name }}
-          </q-item-label>
-        </q-item-section>
+          <q-item-section>
+            <q-item-label
+              :class="{ 'text-strike' : task.completed }"
+            >
+              {{ task.name }}
+            </q-item-label>
+          </q-item-section>
 
-        <q-item-section side>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon
-                class="q-mr-xs"
-                name="event"
-                size="18px"
-              />
-            </div>
+          <q-item-section side>
+            <div class="row">
+              <div class="column justify-center">
+                <q-icon
+                  class="q-mr-xs"
+                  name="event"
+                  size="18px"
+                />
+              </div>
             <div class="column">
               <q-item-label
                 class="row justify-end"
@@ -48,7 +51,7 @@
           </div>
         </q-item-section>
       </q-item>
-
+    </q-list>
   </q-page>
 </template>
 
