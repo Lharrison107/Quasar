87b12682cd83799c508d280d4e0d3a32a56e4b85
Lharrison107 <lharrison@diesellaptops.com>
2022-07-11 12:00:43 -0400

    section 7 complete

--- quasar-project/src/pages/PageTodo.vue
+++ quasar-project/src/pages/PageTodo.vue
@@ -5,63 +5,27 @@
         separator
         bordered
       >
-        <q-item
+        <task
           v-for="(task, key) in tasks"
           :key="key"
-          @click="task.completed = !task.completed"
-          :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-          clickable
-          v-ripple
+          :task="task"
+          :id="key"
         >
-          <q-item-section side top>
-            <q-checkbox v-model="task.completed" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label
-              :class="{ 'text-strike' : task.completed }"
-            >
-              {{ task.name }}
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side>
-            <div class="row">
-              <div class="column justify-center">
-                <q-icon
-                  class="q-mr-xs"
-                  name="event"
-                  size="18px"
-                />
-              </div>
-            <div class="column">
-              <q-item-label
-                class="row justify-end"
-                caption
-              >
-                {{ task.dueDate }}
-              </q-item-label>
-              <q-item-label
-                class="row justify-end"
-                caption
-              >
-                <small>{{ task.dueTime }}</small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+        </task>
     </q-list>
   </q-page>
 </template>
 
 <script>
-import { ref } from 'vue'
 import { mapGetters } from 'vuex'
 
   export default {
     computed: {
      ...mapGetters('tasks', ['tasks'])
+    },
+    components: {
+      'task' : require('components/Tasks/Task.vue').default
+
     }
   }
 </script>
