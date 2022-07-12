52e74ae8f9bc02369c0d2dd239574823a31a9152
Lharrison107 <lharrison@diesellaptops.com>
2022-07-12 10:19:50 -0400

    part 62 complete

--- quasar-project/src/components/Tasks/Modals/AddTask.vue
+++ quasar-project/src/components/Tasks/Modals/AddTask.vue
@@ -17,7 +17,66 @@
        
 
         <q-card-section class="q-pt-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+            <div class="row q-mb-sm">
+                <!-- task name -->
+                <q-input 
+                    outlined 
+                    v-model="taskToSubmit.name" 
+                    label="Task Name" 
+                    class="col"
+                /> 
+            </div>
+            <div class="row q-mb-sm">
+                <!-- task date -->
+                <q-input 
+                    outline 
+                    label="Due Date"
+                    v-model="taskToSubmit.dueDate" 
+                >
+                    <template v-slot:append>
+                        <q-icon 
+                            name="event" 
+                            class="cursor-pointer"
+                        >
+                        <q-popup-proxy 
+                            cover transition-show="scale" 
+                            transition-hide="scale"
+                        >
+                            <q-date v-model="taskToSubmit.dueDate">
+                            <div class="row items-center justify-end">
+                                <q-btn 
+                                    v-close-popup 
+                                    label="Close" 
+                                    color="primary" 
+                                    flat 
+                                />
+                            </div>
+                            </q-date>
+                        </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                </q-input>
+            </div>
+            <div class="row q-mb-sm">
+                <!-- task time -->
+                <q-input 
+                    outlined 
+                    v-model="taskToSubmit.dueTime" 
+                    label="Due Time"
+                >
+                    <template v-slot:append>
+                    <q-icon name="access_time" class="cursor-pointer">
+                        <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                        <q-time v-model="taskToSubmit.dueTime">
+                            <div class="row items-center justify-end">
+                            <q-btn v-close-popup label="Close" color="primary" flat />
+                            </div>
+                        </q-time>
+                        </q-popup-proxy>
+                    </q-icon>
+                    </template>
+                </q-input>
+            </div>
         </q-card-section>
 
         <q-card-actions align="right">
@@ -32,7 +91,18 @@
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                taskToSubmit: {
+                    name: '',
+                    dueDate: '',
+                    dueTime: '',
+                    completed: false
+                }
+            }
+        }
+    }
 </script>
 
 <style>
