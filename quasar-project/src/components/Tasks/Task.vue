a56d87602210476242942781770832f517756e51
Lharrison107 <lharrison@diesellaptops.com>
2022-07-12 08:42:50 -0400

    part 56 completed

--- quasar-project/src/components/Tasks/Task.vue
+++ quasar-project/src/components/Tasks/Task.vue
@@ -21,30 +21,41 @@
         </q-item-section>
 
         <q-item-section side>
-        <div class="row">
-            <div class="column justify-center">
-            <q-icon
-                class="q-mr-xs"
-                name="event"
-                size="18px"
-            />
+            <div class="row">
+                <div class="column justify-center">
+                <q-icon
+                    class="q-mr-xs"
+                    name="event"
+                    size="18px"
+                />
+                </div>
+            <div class="column">
+                <q-item-label
+                class="row justify-end"
+                caption
+                >
+                {{ task.dueDate }}
+                </q-item-label>
+                <q-item-label
+                class="row justify-end"
+                caption
+                >
+                <small>{{ task.dueTime }}</small>
+                </q-item-label>
             </div>
-        <div class="column">
-            <q-item-label
-            class="row justify-end"
-            caption
-            >
-            {{ task.dueDate }}
-            </q-item-label>
-            <q-item-label
-            class="row justify-end"
-            caption
-            >
-            <small>{{ task.dueTime }}</small>
-            </q-item-label>
-        </div>
-        </div>
-    </q-item-section>
+            </div>
+        </q-item-section>
+
+        <q-item-section side>
+            <q-btn 
+                @click.stop="promptToDelete(id)"
+                flat 
+                round 
+                dense
+                color="negative" 
+                icon="delete" 
+            />
+        </q-item-section>
     </q-item>
 </template>
 
@@ -54,7 +65,17 @@ import { mapActions } from 'vuex'
 export default {
     props: ['task', 'id'],
     methods: {
-        ...mapActions('tasks', ['updateTask'])
+        ...mapActions('tasks', ['updateTask']),
+        promptToDelete(id) {
+            this.$q.dialog({
+                title: 'Confirm',
+                message: 'Would you like to delete this task?',
+                cancel: true,
+                persistent: true
+            }).onOk(() => {
+                console.log('deleted')
+            })
+        }
     }
 }
 </script>
