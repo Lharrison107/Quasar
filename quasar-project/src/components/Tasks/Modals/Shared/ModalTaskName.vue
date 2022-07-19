a3702dd501cdb32cd6366abdc4eeccb914df86ef
Lharrison107 <lharrison@diesellaptops.com>
2022-07-19 13:26:44 -0400

    part 101 completed

--- quasar-project/src/components/Tasks/Modals/Shared/ModalTaskName.vue
+++ quasar-project/src/components/Tasks/Modals/Shared/ModalTaskName.vue
@@ -7,6 +7,7 @@
             :rules="[val => !!val || 'Field is required']"
             autofocus 
             ref="name"
+            v-select-all
             label="Task Name" 
             class="col"
         >
@@ -24,7 +25,19 @@
 
 <script>
     export default {
-        props: ['name']
+        props: ['name'],
+        directives: {
+            selectAll: {
+                inserted(el) {
+                    let input = el.querySelector('.q-field__native')
+                    input.addEventListener('focus', () => {
+                        if(input.value.length) {
+                           input.select() 
+                        } 
+                    })
+                }
+            }
+        }
     }
 </script>
 
