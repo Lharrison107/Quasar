f932a74dc9757261d4e633542b859f11cdd5afd6
Lharrison107 <lharrison@diesellaptops.com>
2022-07-21 11:42:53 -0400

    part 113 completed

--- quasar-project/src/pages/PageSettings.vue
+++ quasar-project/src/pages/PageSettings.vue
@@ -1,11 +1,26 @@
 <template>
   <q-page padding>
-    <p>Settings Page</p>
+    <q-separator spaced />
+      <q-item-label header>Settings</q-item-label>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Show 12 hour time format</q-item-label>
+        </q-item-section>
+        <q-item-section side >
+          <q-toggle color="pink" v-model="show12HourTimeFormat" />
+        </q-item-section>
+      </q-item>
   </q-page>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        show12HourTimeFormat: false
+      }
+    }
   }
 </script>
 
