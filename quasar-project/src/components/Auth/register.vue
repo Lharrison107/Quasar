26357e4ecf6c29d55abfd8a8dd52c5b62973f267
Lharrison107 <lharrison@diesellaptops.com>
2022-07-22 16:50:09 -0400

    part 128 completed

--- quasar-project/src/components/Auth/register.vue
+++ quasar-project/src/components/Auth/register.vue
@@ -1,16 +1,55 @@
 <template>
-    <q-banner class="bg-info text-center">
-        <template v-slot:avatar>
-            <q-icon name="how_to_reg" color="white" />
-        </template>
-        <b class="text-white">Create An Account</b>
-    </q-banner>
+    <form>
+        <div class="row q-mb-md">
+            <q-banner class="bg-cyan-1 text-center col">
+                <template v-slot:avatar>
+                    <q-icon name="how_to_reg" color="info" />
+                </template>
+                <b class="text-info">Create An Account</b>
+            </q-banner>
+        </div>
+
+        <div class="row q-mb-md">
+            <q-input
+              class="col"
+              outlined
+              v-model="formData.email"
+              label="Email"
+              stack-label
+            />
+        </div>
+
+        <div class="row q-mb-md">
+            <q-input
+                class="col"
+                outlined
+                v-model="formData.password"
+                type="password"
+                label="Password"
+                stack-label
+            />
+        </div>
+
+        <div class="row q-mb-md">
+            <q-space />
+            <q-btn
+                color="info"
+                label="Create"
+            />
+        </div>
+
+
+    </form>
 </template>
 
 <script>
     export default {
         data() {
             return {
+                formData: {
+                    email: '',
+                    password: ''
+                }
             }
         },
         methods: {
