4a0fc71b10b0121899404374579c063ec5b92843
Lharrison107 <lharrison@diesellaptops.com>
2022-07-22 11:04:34 -0400

    part 118 completed

--- quasar-project/src/store/store.settings.js
+++ quasar-project/src/store/store.settings.js
@@ -1,3 +1,5 @@
+import { LocalStorage } from 'quasar';
+
 const state = {
     settings: {
         show12HourTimeFormat: false,
@@ -11,16 +13,31 @@ const mutations = {
     },
     setShowTasksInOneList(state, value) {
         state.settings.showTasksInOneList = value
-    }
+    },
+    setSettings(state, settings) {
+		Object.assign(state.settings, settings)
+	}
 }
 
 const actions = {
-    setShow12HourTimeFormat({commit}, value) {
+    setShow12HourTimeFormat({commit, dispatch}, value) {
         commit('setShow12HourTimeFormat', value)
+        dispatch('saveSettings')
     },
-    setShowTasksInOneList({commit}, value) {
+    setShowTasksInOneList({commit, dispatch}, value) {
         commit('setShowTasksInOneList', value)
-    }
+        dispatch('saveSettings')
+    },
+    saveSettings({ state }) {
+		LocalStorage.set('settings', state.settings)
+        console.log(LocalStorage.settings)
+	},
+    getSettings({ commit }) {
+		let settings = LocalStorage.getItem('settings')
+		if (settings) {
+			commit('setSettings', settings)
+		}
+	}
 }
 
 const getters = {
