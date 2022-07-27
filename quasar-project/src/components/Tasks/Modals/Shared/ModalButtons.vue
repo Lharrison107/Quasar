7212a6cfd0e0b7d2d0dd0fbc7dc3a905beae6f68
Lharrison107 <lharrison@diesellaptops.com>
2022-07-27 13:41:43 -0400

    got the page to reload when add, edit, or deleteing a task still need to figure out refresh on login

--- quasar-project/src/components/Tasks/Modals/Shared/ModalButtons.vue
+++ quasar-project/src/components/Tasks/Modals/Shared/ModalButtons.vue
@@ -4,12 +4,18 @@
             type="submit"
             label="save" 
             color="primary"
+            @click="reloadPage"
         />
     </q-card-actions>
 </template>
 
 <script>
     export default {
+        methods: {
+            reloadPage() {
+                window.location.reload()
+            }
+        }
     }
 </script>
 
